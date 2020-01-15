caecf6dbad4d6945d3e275d7f3262bcc56ad199b
Jan Boček <honza.bocek@gmail.com>
2020-01-15 22:56:59 +0100

    doplněna legenda

--- article.md
+++ article.md
@@ -6,6 +6,16 @@ styles: []
 libraries: ['https://code.jquery.com/jquery-1.11.3.js', 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js']
 options: [] #wide, noheader (, nopic)
 ---
-<div id="koberec1"></div>
 
-<div id="koberec2"></div>
+Jiří Čunek (KDU-ČSL), Jiří Dienstbier (ČSSD), Ivo Valenta (ODS) nebo Jaroslav Zeman (STAN): to jsou senátoři, kteří často volí jinak než jejich klub.
+
+<wide><div><img src="https://dev.datarozhlas.cz/senat-kdoskym/grafy/kdoskym2000.png" style="width:100%"></div></wide>
+
+Ukázala to data ze všech 589 hlasování v aktuálním senátním období, tedy od podzimu 2018 do dneška. [Fyzikální model](https://en.wikipedia.org/wiki/Force-directed_graph_drawing) umístí spojence při hlasování blíž k sobě, a naopak: ti, kdo často hlasují odlišeně, jsou si také v grafu vzdálenější.
+
+
+<wide><div><img src="https://dev.datarozhlas.cz/senat-kdoskym/grafy/kdoskymuzsi2000.png" style="width:100%"></div></wide>
+
+<wide><div id="koberec1"></div></wide>
+
+<!--wide><div id="koberec2"></div></wide-->
--- js/script.js
+++ js/script.js
@@ -1,48 +1,42 @@
 ﻿import "./byeie"; // loučíme se s IE
 
-fetch("https://dev.datarozhlas.cz/senat-kdoskym/grafy/koalice.csv")
+fetch("./grafy/vyb_koalice.csv")
 .then(response => response.text())
 .then(textkoal => {
 
-fetch("https://dev.datarozhlas.cz/senat-kdoskym/grafy/opozice.csv")
+fetch("./grafy/vyb_opozice.csv")
 .then(response => response.text())
 .then(textopoz => {
 
-fetch("https://dev.datarozhlas.cz/senat-kdoskym/grafy/tooltipy.csv")
+fetch("./grafy/vyb_tooltipy.csv")
 .then(response => response.text())
 .then(tooltipy => {
 
-var datakoal = JSON.parse("[" + textkoal + "]");
+  var datakoal = JSON.parse("[" + textkoal + "]");
 
-var dataopoz = JSON.parse("[" + textopoz + "]");
+  var dataopoz = JSON.parse("[" + textopoz + "]");
 
-var tooltipy = tooltipy.split('\n');
+  var tooltipy = tooltipy.split('\n');
 
-var datatt = [];
-var headers = tooltipy[0].split('|');
+  var datatt = [];
+  var headers = tooltipy[0].split('|');
 
-for (var i = 1; i < tooltipy.length; i++) {
-
-	var datatemp = tooltipy[i].split('|');
-	var objtemp = {};
-
-	for (var j = 0; j < datatemp.length; j++) {
-
-		objtemp[headers[j].trim()] = datatemp[j].trim();
-
-	}
-
-	datatt.push(objtemp);
-
+  for (var i = 1; i < tooltipy.length; i++) {
+    var datatemp = tooltipy[i].split('|');
+    var objtemp = {};
+    for (var j = 0; j < datatemp.length; j++) {
+      objtemp[headers[j].trim()] = datatemp[j].trim();
+    }
+    datatt.push(objtemp);
 }
 
 datatt.pop();
 
-var square = 8;
+var square = 6;
 
 var config = {
 	width: 81 * square,
-	height: 589 * square,
+	height: tooltipy.length * square,
 	title: 30,
 	grid: {
 		width: square,
@@ -54,114 +48,259 @@ var config = {
 };
 
 function getPosition(dataIndex) {
-
 	if(!dataIndex) return {x: 5, y: config.title + 5};
 
 	var nthColumn = dataIndex % (config.width / config.grid.width),
 		  nthRow = Math.floor(dataIndex / (config.height / config.grid.height / (config.height / config.width)));
 
 	var columnWidth = config.grid.width,
-		rowHeight = config.grid.height;
-
-		return {
-			x: columnWidth * nthColumn + 5,
-			y: rowHeight * nthRow + config.title + 5
-		};
-
+      rowHeight = config.grid.height;
+      return {
+        x: columnWidth * nthColumn + 5,
+        y: rowHeight * nthRow + config.title + 5
+      };
 }
 
 function renderSVG(data, tooltip, box) {
 
-	var svg = d3.select('#koberec1')
-				.append('svg')
-				.attr({
-					width: config.width + 5,
-					height: config.height + config.title + 5
-				})
+	var odsunx = 260,
+		odsuny = 2990;
 
-	var div = d3.select('#koberec1').append('div')
-				.attr('class', 'tooltip')
-				.style('opacity', 0);
+    var svg = d3.select('#koberec1')
+                .append('svg')
+                .attr({
+                  width: config.width + 5,
+                  height: config.height + config.title + 5
+                })
 
-	svg.selectAll('rect').data(data).enter().append('rect').attr({
-		width: config.grid.width - 2,
-		height: config.grid.height - 2,
-		x: function(d, i) { return getPosition(i).x; },
-		y: function(d, i) { return getPosition(i).y; },
-		fill: function(d) { return config.colors[d]; }
-	})
+    var div = d3.select('#koberec1').append('div')
+                .attr('class', 'tooltip')
+                .style('opacity', 0);
 
-	var row = svg.selectAll('row').data(tooltip).enter().append('rect').attr({
-		width: 81 * square + 2,
-		height: 10,
-		x: 3,
-		y: function(d, i) { return (config.grid.height * i) + config.title + 3; },
-		fill: '#fff'
-	}).style('fill-opacity', 0)
-		.style('opacity', 0)
-		.style('stroke', 'black')
-		.style('stroke-width', 1)
+    svg.selectAll('rect').data(data).enter().append('rect').attr({
+      width: config.grid.width - 1,
+      height: config.grid.height - 1,
+      x: function(d, i) { return getPosition(i).x; },
+      y: function(d, i) { return getPosition(i).y; },
+      fill: function(d) { return config.colors[d]; }
+    })
 
-	row.on('mouseover', function(d) {
+    var row = svg.selectAll('row').data(tooltip).enter().append('rect').attr({
+      width: 81 * square + 2,
+      height: 10,
+      x: 3,
+      y: function(d, i) { return (config.grid.height * i) + config.title + 3; },
+      fill: '#fff'
+    }).style('fill-opacity', 0)
+      .style('opacity', 0)
+      .style('stroke', 'black')
+      .style('stroke-width', 1)
 
-		div.transition()
-			.duration(200)
-			.style('opacity', .9);
+    row.on('mouseover', function(d) {
+        div.transition()
+           .duration(200)
+           .style('opacity', .9);
+        var vysledek;
+        if(d.vysledek == 'Přijato') {
+          vysledek = '<div class="ttcajk">Schváleno</div>';
+        } else if (d.vysledek == 'Zamítnuto') {
+          vysledek = '<div class="ttnecajk">Zamítnuto</div>';
+        } else {
+          vysledek = '<div class="ttborcus">Zmatečné</div>';
+        }
+        var nazev;
+        if(d.nazev.length < 150) {
+          nazev = '<div class="ttheader">' + d.nazev + '</div>'
+        } else {
+          nazev = '<div class="ttheader">' + d.nazev.substring(0, 150).split(' ').slice(0, -1).join(' ') + '…</div>'
+        }
+        div.html(nazev + vysledek +
+                 '<div class="ttvotes">' + d.kvorum + ' hlasů ke schválení</div>' +
+                 '<div class="ttvotes">' + d.pro + ' pro — ' +
+                 d.proti + ' proti — ' +
+                 d.zdrzeli + ' se zdrželo — ' +
+                 d.absence + ' nepřítomných</div>')
+           .style('left', (d3.event.pageX - odsunx + 20) + 'px')
+           .style('top', (d3.event.pageY - odsuny + 30) + 'px');
+        d3.select(this)
+          .style('fill-opacity', 0.5)
+          .style('opacity', 1)
+          .transition()
+          .duration(200)
+      })
+      .on('mouseout', function(d) {
+        div.transition()
+           .duration(500)
+           .style('opacity', 0);
+        d3.select(this)
+          .style('opacity', 0)
+      });
 
-		var vysledek;
-
-		if(d.vysledek == 'Přijato') {
-			vysledek = '<div class="ttcajk">Schváleno</div>';
-		} else if (d.vysledek == 'Zamítnuto') {
-			vysledek = '<div class="ttnecajk">Zamítnuto</div>';
-		} else {
-			vysledek = '<div class="ttborcus">Zmatečné</div>';
-		}
-
-		var nazev;
-
-		if(d.nazev.length < 150) {
-			nazev = '<div class="ttheader">' + d.nazev + '</div>'
-		} else {
-			nazev = '<div class="ttheader">' + d.nazev.substring(0, 150).split(' ').slice(0, -1).join(' ') + '…</div>'
-		}
-
-		div.html(nazev + vysledek +
-				'<div class="ttvotes">' + d.kvorum + ' hlasů ke schválení</div>' +
-				'<div class="ttvotes">' + d.pro + ' pro — ' +
-				d.proti + ' proti — ' +
-				d.zdrzeli + ' se zdrželo — ' +
-				d.absence + ' nepřítomných</div>')
-			.style('left', (d3.event.pageX + 20) + 'px')
-			.style('top', (d3.event.pageY + 30) + 'px');
-
-		d3.select(this)
-			.style('fill-opacity', 0.5)
-			.style('opacity', 1)
-			.transition()
-			.duration(200)
-		}).on('mouseout', function(d) {
-
-		div.transition()
-			.duration(500)
-			.style('opacity', 0);
-
-		d3.select(this)
-			.style('opacity', 0)
-		});
-
-		var text = svg.append("text")
-						.attr("x", config.width/2)
-						.attr("y", 20)
-						.attr("text-anchor", "middle")
-						.attr("class", "title")
-						.text(box);
-
+      var text = svg.append("text")
+                    .attr("x", config.width/3)
+                    .attr("y", 20)
+                    .attr("text-anchor", "middle")
+                    .attr("class", "title")
+                    .text(box)
 }
 
-renderSVG(datakoal, datatt, 'VÍTĚZOVÉ HLASOVÁNÍ');
+    renderSVG(datakoal, datatt, 'VÍTĚZOVÉ HLASOVÁNÍ');
 
-renderSVG(dataopoz, datatt, 'PORAŽENÍ');
+    renderSVG(dataopoz, datatt, 'PORAŽENÍ');
+
+});
+
+});
+
+});
+
+fetch("./grafy/koalice.csv")
+.then(response => response.text())
+.then(textkoal => {
+
+fetch("./grafy/opozice.csv")
+.then(response => response.text())
+.then(textopoz => {
+
+fetch("./grafy/tooltipy.csv")
+.then(response => response.text())
+.then(tooltipy => {
+
+  var datakoal = JSON.parse("[" + textkoal + "]");
+
+  var dataopoz = JSON.parse("[" + textopoz + "]");
+
+  var tooltipy = tooltipy.split('\n');
+
+  var datatt = [];
+  var headers = tooltipy[0].split('|');
+
+  for (var i = 1; i < tooltipy.length; i++) {
+    var datatemp = tooltipy[i].split('|');
+    var objtemp = {};
+    for (var j = 0; j < datatemp.length; j++) {
+      objtemp[headers[j].trim()] = datatemp[j].trim();
+    }
+    datatt.push(objtemp);
+}
+
+datatt.pop();
+
+var square = 6;
+
+var config = {
+	width: 81 * square,
+	height: tooltipy.length * square,
+	title: 30,
+	grid: {
+		width: square,
+		height: square
+	},
+	diff: 1,
+	// 1 STAN, 2 ODS, 3 KDU-ČSL + nez., 4 ČSSD, 5 ANO, 6 Senátor 21, 7 nezařazení
+	colors: ['#FFFFFF', '#A8CB4A', '#009BD5', '#FFDA05', '#F29400', '#272660', '#CB2D42', '#A0A0A0']
+};
+
+function getPosition(dataIndex) {
+	if(!dataIndex) return {x: 5, y: config.title + 5};
+
+	var nthColumn = dataIndex % (config.width / config.grid.width),
+		  nthRow = Math.floor(dataIndex / (config.height / config.grid.height / (config.height / config.width)));
+
+	var columnWidth = config.grid.width,
+      rowHeight = config.grid.height;
+      return {
+        x: columnWidth * nthColumn + 5,
+        y: rowHeight * nthRow + config.title + 5
+      };
+}
+
+function renderSVG(data, tooltip, box) {
+
+	var odsunx = 260,
+		odsuny = 900;
+
+    var svg = d3.select('#koberec2')
+                .append('svg')
+                .attr({
+                  width: config.width + 5,
+                  height: config.height + config.title + 5
+                })
+
+    var div = d3.select('#koberec2').append('div')
+                .attr('class', 'tooltip')
+                .style('opacity', 0);
+
+    svg.selectAll('rect').data(data).enter().append('rect').attr({
+      width: config.grid.width - 1,
+      height: config.grid.height - 1,
+      x: function(d, i) { return getPosition(i).x; },
+      y: function(d, i) { return getPosition(i).y; },
+      fill: function(d) { return config.colors[d]; }
+    })
+
+    var row = svg.selectAll('row').data(tooltip).enter().append('rect').attr({
+      width: 81 * square + 2,
+      height: 10,
+      x: 3,
+      y: function(d, i) { return (config.grid.height * i) + config.title + 3; },
+      fill: '#fff'
+    }).style('fill-opacity', 0)
+      .style('opacity', 0)
+      .style('stroke', 'black')
+      .style('stroke-width', 1)
+
+    row.on('mouseover', function(d) {
+        div.transition()
+           .duration(200)
+           .style('opacity', .9);
+        var vysledek;
+        if(d.vysledek == 'Přijato') {
+          vysledek = '<div class="ttcajk">Schváleno</div>';
+        } else if (d.vysledek == 'Zamítnuto') {
+          vysledek = '<div class="ttnecajk">Zamítnuto</div>';
+        } else {
+          vysledek = '<div class="ttborcus">Zmatečné</div>';
+        }
+        var nazev;
+        if(d.nazev.length < 150) {
+          nazev = '<div class="ttheader">' + d.nazev + '</div>'
+        } else {
+          nazev = '<div class="ttheader">' + d.nazev.substring(0, 150).split(' ').slice(0, -1).join(' ') + '…</div>'
+        }
+        div.html(nazev + vysledek +
+                 '<div class="ttvotes">' + d.kvorum + ' hlasů ke schválení</div>' +
+                 '<div class="ttvotes">' + d.pro + ' pro — ' +
+                 d.proti + ' proti — ' +
+                 d.zdrzeli + ' se zdrželo — ' +
+                 d.absence + ' nepřítomných</div>')
+           .style('left', (d3.event.pageX - odsunx + 20) + 'px')
+           .style('top', (d3.event.pageY - odsuny + 30) + 'px');
+        d3.select(this)
+          .style('fill-opacity', 0.5)
+          .style('opacity', 1)
+          .transition()
+          .duration(200)
+      })
+      .on('mouseout', function(d) {
+        div.transition()
+           .duration(500)
+           .style('opacity', 0);
+        d3.select(this)
+          .style('opacity', 0)
+      });
+
+      var text = svg.append("text")
+                    .attr("x", config.width/3)
+                    .attr("y", 20)
+                    .attr("text-anchor", "middle")
+                    .attr("class", "title")
+                    .text(box)
+}
+
+    renderSVG(datakoal, datatt, 'VÍTĚZOVÉ HLASOVÁNÍ');
+
+    renderSVG(dataopoz, datatt, 'PORAŽENÍ');
 
 });
 

e4c06f502a5eb96d7380d32ee10734ceb3c47ff5
datastory.cz <honza@datastory.cz>
2020-01-16 14:28:58 +0100

    tooltip mobile last :(

--- js/script.js
+++ js/script.js
@@ -45,11 +45,6 @@ function renderSVG(data, tooltip, box, divId, odsunx, odsuny) {
 					height: config.height + config.title + 5
                 })
 
-	var div = d3.select(divId)
-				.append('div')
-                .attr('class', 'tooltip')
-                .style('opacity', 0);
-
 	svg.selectAll('rect')
 		.data(data)
 		.enter()
@@ -77,54 +72,65 @@ function renderSVG(data, tooltip, box, divId, odsunx, odsuny) {
 				.style('opacity', 0)
 				.style('stroke', 'black')
 				.style('stroke-width', 1)
+	
+	if (document.getElementById("koberec1").offsetWidth >= 1000) { // mobil bez tooltipu, sry :/
+		var div = d3.select(divId)
+		.append('div')
+		.attr('class', 'tooltip')
+		.style('opacity', 0)
 
-    row.on('mouseover', function(d) {
-		div.transition()
-			.duration(200)
-			.style('opacity', .9)
-			.style('z-index', 300)
-		
-		var vysledek
-		if (d.vysledek == 'Přijato') {
-			vysledek = '<div class="ttcajk">Schváleno</div>'
-		} else if (d.vysledek == 'Zamítnuto') {
-			vysledek = '<div class="ttnecajk">Zamítnuto</div>'
-		} else {
-			vysledek = '<div class="ttborcus">Zmatečné</div>'
-		}
-		
-		var nazev
-		if (d.nazev.length < 150) {
-			nazev = '<div class="ttheader">' + d.nazev + '</div>'
-		} else {
-			nazev = '<div class="ttheader">' + d.nazev.substring(0, 150).split(' ').slice(0, -1).join(' ') + '…</div>'
-		}
+		row.on('mouseover', function(d) {
+			div.transition()
+				.duration(200)
+				.style('opacity', .9)
+				.style('z-index', 300)
+			
+			var vysledek
+			if (d.vysledek == 'Přijato') {
+				vysledek = '<div class="ttcajk">Schváleno</div>'
+			} else if (d.vysledek == 'Zamítnuto') {
+				vysledek = '<div class="ttnecajk">Zamítnuto</div>'
+			} else {
+				vysledek = '<div class="ttborcus">Zmatečné</div>'
+			}
+			
+			var nazev
+			if (d.nazev.length < 150) {
+				nazev = '<div class="ttheader">' + d.nazev + '</div>'
+			} else {
+				nazev = '<div class="ttheader">' + d.nazev.substring(0, 150).split(' ').slice(0, -1).join(' ') + '…</div>'
+			}
+	
+			div.html(nazev + vysledek 
+				+ '<div class="ttvotes">' + d.kvorum + ' hlasů ke schválení</div>' 
+				+ '<div class="ttvotes">' 
+					+ d.pro + ' pro — ' 
+					+ d.proti + ' proti — ' 
+					+ d.zdrzeli + ' se zdrželo — '
+				+ d.absence + ' nepřítomných</div>'
+			)
+			div.style('left', (Math.abs(d3.mouse(this)[0]) - odsunx + 20) + 'px')
+				.style('top', (Math.abs(d3.mouse(this)[1]) - odsuny + 30) + 'px')
+			
+			d3.select(this)
+				.style('fill-opacity', 0.5)
+				.style('opacity', 1)
+				.transition()
+				.duration(200)
+		})
+		  
+		row.on('mouseout', function(d) {
+			div.transition()
+			   .duration(500)
+			   .style('opacity', 0);
+			d3.select(this)
+			  .style('opacity', 0)
+		})
+	}
+	
+	
 
-		div.html(nazev + vysledek 
-			+ '<div class="ttvotes">' + d.kvorum + ' hlasů ke schválení</div>' 
-			+ '<div class="ttvotes">' 
-				+ d.pro + ' pro — ' 
-				+ d.proti + ' proti — ' 
-				+ d.zdrzeli + ' se zdrželo — '
-			+ d.absence + ' nepřítomných</div>'
-		)
-		div.style('left', (Math.abs(d3.mouse(this)[0]) - odsunx + 20) + 'px')
-			.style('top', (Math.abs(d3.mouse(this)[1]) - odsuny + 30) + 'px')
-		
-		d3.select(this)
-			.style('fill-opacity', 0.5)
-			.style('opacity', 1)
-			.transition()
-			.duration(200)
-    })
-	  
-	row.on('mouseout', function(d) {
-        div.transition()
-           .duration(500)
-           .style('opacity', 0);
-        d3.select(this)
-          .style('opacity', 0)
-      })
+    
 
     svg.append("text")
 		.attr("x", config.width/3)
